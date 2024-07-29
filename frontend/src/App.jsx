aa39d50366c16420b6d840d9945915902d78a16b
Grant <grantimaxi03@gmail.com>
2024-07-29 02:13:22 -0400

    Create map function to load multiple images from an array

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,45 @@
 import React from 'react';
-
 import PhotoListItem from './components/PhotoListItem';
 import './App.scss';
 
-const sampleDataForPhotoListItem = {
-  id: "1",
-  location: {
-    city: "Montreal",
-    country: "Canada",
+const sampleDataForPhotoListItems = [
+  {
+    id: "1",
+    location: {
+      city: "Montreal",
+      country: "Canada",
+    },
+    profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
+    imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
+    username: "Joe Example",
   },
-  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-  username: "Joe Example",
-};
+  {
+    id: "2",
+    location: {
+      city: "Montreal",
+      country: "Canada",
+    },
+    profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
+    imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
+    username: "Joe Example",
+  },
+  {
+    id: "3",
+    location: {
+      city: "Montreal",
+      country: "Canada",
+    },
+    profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
+    imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
+    username: "Joe Example",
+  },
+];
 
 const App = () => (
   <div className="App">
-    <PhotoListItem photo={sampleDataForPhotoListItem} />
+    {sampleDataForPhotoListItems.map(photo => (
+      <PhotoListItem key={photo.id} photo={photo} />
+    ))}
   </div>
 );
 
