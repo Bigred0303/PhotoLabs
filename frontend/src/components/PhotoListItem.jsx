e8c935419528eeb71b4290f3e0816f819a83f8cb
Grant <grantimaxi03@gmail.com>
2024-07-29 00:38:28 -0400

    Create basic styling for photolistitem

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -1,29 +1,34 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import 'styles/PhotoListItem.scss';
 
 const PhotoListItem = ({ photo }) => {
   const { id, location, imageSource, username, profile } = photo;
   return (
-    <div className="photo-list-item">
-      <img src={profile} alt={`${username}'s profile`} className="profile-pic"/>
-      <div className="photo-details">
-        <h2>{username}</h2>
-        <p>{location.city}, {location.country}</p>
-        <img src={imageSource} alt={`Photo by ${username}`} className="photo"/>
+    <div className="photo-list__item">
+      <img src={imageSource} alt={`Photo by ${username}`} className="photo-list__image"/>
+      <div className="photo-list__user-details">
+        <img src={profile} alt={`${username}'s profile`} className="photo-list__user-profile"/>
+        <div className="photo-list__user-info">
+          <div>{username}</div>
+          <div className="photo-list__user-location">{location.city}, {location.country}</div>
+        </div>
       </div>
     </div>
   );
 };
 
 PhotoListItem.propTypes = {
-  id: PropTypes.string.isRequired,
-  location: PropTypes.shape({
-    city: PropTypes.string.isRequired,
-    country: PropTypes.string.isRequired,
+  photo: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    location: PropTypes.shape({
+      city: PropTypes.string.isRequired,
+      country: PropTypes.string.isRequired,
+    }).isRequired,
+    imageSource: PropTypes.string.isRequired,
+    username: PropTypes.string.isRequired,
+    profile: PropTypes.string.isRequired,
   }).isRequired,
-  imageSource: PropTypes.string.isRequired,
-  username: PropTypes.string.isRequired,
-  profile: PropTypes.string.isRequired,
 };
 
 export default PhotoListItem;
